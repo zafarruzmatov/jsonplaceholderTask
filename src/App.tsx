4063b37162eaf3bc458ee48e0c9651e0eb137799
zafarruzmatov <zrozmatov0@gmail.com>
2024-03-16 16:38:11 +0500

    feat: shadnc-ui installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-    return <h1>Jsonplaceholder Task</h1>;
+    return (
+        <>
+            <h1>Jsonplaceholder Task</h1>
+            <Button>Click</Button>
+        </>
+    );
 }
 
 export default App;
