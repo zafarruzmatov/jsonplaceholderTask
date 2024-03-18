0bb4be595c31cee5d5cd303d28090ce7a8a1b31e
zafarruzmatov <zrozmatov0@gmail.com>
2024-03-18 14:30:47 +0500

    refactor: userResponse change

--- src/pages/users/type.ts
+++ src/pages/users/type.ts
@@ -1,6 +1,27 @@
+interface IAddress {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+        lat: string;
+        lng: string;
+    };
+}
+
+interface ICompany {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+
 export interface IUserResponseGet {
     id: number;
     name: string;
     username: string;
     email: string;
+    address: IAddress;
+    phone: string;
+    website: string;
+    company: ICompany;
 }
